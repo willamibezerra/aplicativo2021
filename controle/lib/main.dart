78ccd70e018ae56aaa458b56b435ac756f5dfc47
bezerrawillami <bezerrawillami@gmail.com>
2021-09-14 10:17:47 -0300

    a estrutura do codigo e a ordem do calculo

--- controle/lib/main.dart
+++ controle/lib/main.dart
@@ -9,47 +9,44 @@ void main() {
 }
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   TextEditingController firstcontroller = TextEditingController();
   TextEditingController secuntcontroller = TextEditingController();
   TextEditingController operationcontroller = TextEditingController();
-  double _calcular= 0;
+  double _calcular = 0;
 
-
-  void _reset(){
-    firstcontroller.text= "";
-    secuntcontroller.text= "";
-    operationcontroller.text="";
+  void _reset() {
+    firstcontroller.text = "";
+    secuntcontroller.text = "";
+    operationcontroller.text = "";
     setState(() {
-       _calcular= 0;
+      _calcular = 0;
     });
   }
-void calculo(){
-  setState(() {
-    double first = double.parse(firstcontroller.text);
-    double secunt = double.parse(secuntcontroller.text);
-    String operation = operationcontroller.text;
 
-    if (operation=="/"){
-      _calcular = first/secunt;
-    }else if(operation == "+" ){
-      _calcular = first+secunt;
-    }else if (operation== "-" ){
-      _calcular= first- secunt;
-    }else if (operation== "*"){
-      _calcular= first*secunt;
-    }else{
-      _calcular= 0;
-    }
+  void calculo() {
+    setState(() {
+      double first = double.parse(firstcontroller.text);
+      double secunt = double.parse(secuntcontroller.text);
+      String operation = operationcontroller.text;
 
-  });
-}
+      if (operation == "+") {
+        _calcular = first + secunt;
+      } else if (operation == "/") {
+        _calcular = first / secunt;
+      } else if (operation == "-") {
+        _calcular = first - secunt;
+      } else if (operation == "*") {
+        _calcular = first * secunt;
+      } else {
+        _calcular = 0;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,17 +112,21 @@ void calculo(){
                 ),
                 Image.network(
                     "http://4.bp.blogspot.com/_tfjeykz8M_k/Sh3rDG_wsxI/AAAAAAAADAo/swwMxK0dgOU/s640/homer_simpson_pensando.jpg"),
-            SizedBox(
-              height: 20,),
-                Text( "o resultado é $_calcular", style: TextStyle(color: Colors.blue),),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "o resultado é $_calcular",
+                  style: TextStyle(color: Colors.blue),
+                ),
                 SizedBox(
                   height: 100,
                 ),
                 Container(
                   width: 1000,
                   height: 40,
-                  child:
-                      ElevatedButton(onPressed: calculo, child: Text("calcular")),
+                  child: ElevatedButton(
+                      onPressed: calculo, child: Text("calcular")),
                 )
               ],
             ),
